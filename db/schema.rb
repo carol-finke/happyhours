e913d5da2d4544dc7eecacc946e1669e9fe57b3e
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-06-10 02:56:06 +0000

    Updated Gemfile, removed posts_controller, updated models and routes, added happy_hours controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_154954) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_024416) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_154954) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "users", force: :cascade do |t|
